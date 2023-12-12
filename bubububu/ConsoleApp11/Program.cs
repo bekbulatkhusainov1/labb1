8437c002e75701479d26f348f21220446c7437ac
Dzemaaa <bekbulatkhusainov2006@gmail.com>
2023-12-12 22:36:08 +0400

    labb1

---
+++ bubububu/ConsoleApp11/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите натуральное число для проверки на палиндром:");
+            string input = Console.ReadLine();
+            int number;
+
+            if (int.TryParse(input, out number))
+            {
+                if (IsPalindrome(number))
+                {
+                    Console.WriteLine($"{number} - является палиндромом.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} - не является палиндромом.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введено некорректное значение.");
+            }
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            string numberString = number.ToString();
+            int length = numberString.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (numberString[i] != numberString[length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+
+    }
+}
